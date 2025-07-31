bf53466c0fd0da061a1d0308c0a488b15214ce0d
simone-poli <polismn97@gmail.com>
2025-07-31 16:31:51 +0200

    connect with database

--- database/connection.js
+++ database/connection.js
@@ -1,4 +1,4 @@
-const connection = require('mysql2')
+const mysql = require('mysql2')
 
 const credentials = {
     host: process.env.DB_HOST,
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT
 
-//const connection = require('./database/connection')
+const connection = require('./database/connection')
 
 
 app.listen(PORT, () => {
@@ -19,13 +19,51 @@ app.get('/', (req,res) => {
 
 //INDEX
 app.get('/api/movies', (req, res) => {
-    res.send("List of movies")
+    const sql = 'SELECT * FROM movies';
+
+    connection.query(sql, (err, result)=> {
+        if (err) return res.status(500).json({
+            err: true,
+            message: err.message
+        })
+        console.log(result)
+        res.json(result)
+    })
+    
 })
 
 
 //SHOW
 app.get('/api/movies/:id', (req, res) => {
-    const id = req.params.id;
-    res.send(`This is film with id: ${id}`)
+    const id = Number(req.params.id);
+    const sql= `SELECT * FROM movies WHERE id=?`
+
+    connection.query(sql, [id], (err,result) =>{
+        if(err) return res.status(500).json({
+            error: true,
+            message: err.message
+        })
+       
+        if (result.length === 0) {
+            return res.status(404).json({
+                error: true,
+                message: 'Not found'
+            })
+        }
+        const movie = result[0]
+
+        const reviewSql = 'SELECT * FROM reviews WHERE movie_id = ?'
+        connection.query(reviewSql , [id], (err,result) =>{
+            if(err) return res.status(500).json({
+                error: true,
+                message: err.message
+            })
+            const movieReviews = result
+            movie.reviews = movieReviews
+            console.log(movie)
+            res.json(movie)
+        })
+    })
+
 })
 
